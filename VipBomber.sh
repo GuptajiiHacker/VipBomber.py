52aca3b672024eb933a52ebbddeb1dbf517d8cca
GuptajiiHacker <155041576+GuptajiiHacker@users.noreply.github.com>
2023-12-30 09:27:59 +0530

    VipBomber.sh

--- VipBomber.sh
+++ VipBomber.sh
@@ -27,21 +27,21 @@ banner() {
     clear
     echo -e "\e[1;31m"
     if ! [ -x "$(command -v figlet)" ]; then
-        echo 'Introducing X_BOMB'
+        echo 'Introducing TBomb'
     else
-        figlet X_BOMB
+        figlet TBomb
     fi
     if ! [ -x "$(command -v toilet)" ]; then
-        echo -e "\e[4;34m This Bomber Was Created By \e[1;32mhackerxphantom\e[0m"
+        echo -e "\e[4;34m This Bomber Was Created By \e[1;32mSpeedX \e[0m"
     else
         echo -e "\e[1;34mCreated By \e[1;34m"
-        toilet -f mono12 -F border X_PH4N70M
+        toilet -f mono12 -F border SpeedX
     fi
     echo -e "\e[1;34m For Any Queries Join Me!!!\e[0m"
-    echo -e "\e[1;32m           Telegram: https://t.me/X_PH4N7OM \e[0m"
-    echo -e "\e[4;32m   JOIN US: https://bit.ly/3PV3S3r \e[0m"
+    echo -e "\e[1;32m           Telegram: https://t.me/TBombChat \e[0m"
+    echo -e "\e[4;32m   YouTube: https://www.youtube.com/c/SpeedXTech \e[0m"
     echo " "
-    echo "NOTE: Kindly move to the PIP version Of X_BOMB for more stability."
+    echo "NOTE: Kindly move to the PIP version Of TBomb for more stability."
     echo " "
 }
 
@@ -98,7 +98,7 @@ else
     echo .
     echo .
     install_deps
-    echo This Script Was Made By X PHANTOM X_PH4N7OM > .update
+    echo This Script Was Made By SpeedX > .update
     echo 'Requirements Installed....'
     pause
 fi
@@ -113,7 +113,6 @@ do
     echo "Press 4 To  Update (Works On Linux And Linux Emulators) "
     echo "Press 5 To  Exit "
     read ch
-    echo "select option "
     clear
     if [ $ch -eq 1 ];then
         $PYTHON bomber.py --sms
@@ -128,7 +127,7 @@ do
         echo -e "\e[1;34m Downloading Latest Files..."
         rm -f .update
         $PYTHON bomber.py --update
-        echo -e "\e[1;34m RUN X_BOMB Again..."
+        echo -e "\e[1;34m RUN TBomb Again..."
         pause
         exit
     elif [ $ch -eq 5 ];then
